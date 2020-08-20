8fd5087a25242d6d4e11c56da078f488c7a7fce5
João Vitor <joao.vpf@gmail.com>
2020-08-20 11:40:19 -0300

    Alteração de estrutura

---
+++ includes/scripts.php
@@ -0,0 +1,11 @@
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- overlayScrollbars -->
+<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+
+<!-- General -->
+<script src="dist/js/adminlte.js"></script>
